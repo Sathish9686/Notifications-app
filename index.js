774fe6018ae9f2decdf0347a9bdfe6977806a9e3
Sathish S <126003107+Sathish9686@users.noreply.github.com>
2023-03-16 21:17:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-const Notification = props => {
-  
-        const {Icon, text , imgUrl} = props;
-        return (
-            <div>
-                <img src={imgUrl}/>
-                <p className={Icon}>{text}</p>
-            </div>
-        )
-}
+const Notification = (props) => {
+  const { className, imgUrl, notfnText, imgIcon, textSize } = props;
+  return (
+    <div className={className}>
+      <img src={imgUrl} className={imgIcon} />
+      <p className={`${textSize}`}>{notfnText} </p>
+    </div>
+  );
+};
 
 const element = (
+  <div className="bg-container">
+    <h1>Notifications</h1>
+    <div className="Notification-container">
+      <Notification
+        className="Ntfn-1"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+        notfnText="Information Message"
+        imgIcon="image-icon"
+        textSize="text"
+      />
 
-  <div className="main-bg-container">
-      <div className="notification-container">
-        <div>
-            <Notification Icon="Notfication1", text="information Message" imgUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"/>
-        </div>
-      </div>
+      <Notification
+        className="Ntfn-2"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+        notfnText="Success Message"
+        imgIcon="image-icon"
+        textSize="text"
+      />
+
+      <Notification
+        className="Ntfn-3"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+        notfnText="Warning Message"
+        imgIcon="image-icon"
+        textSize="text"
+      />
+
+      <Notification
+        className="Ntfn-4"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+        notfnText="Error Message"
+        imgIcon="image-icon"
+        textSize="text"
+      />
+    </div>
   </div>
-)
+);
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById("root"));
